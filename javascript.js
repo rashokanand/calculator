f33f6fbc8fadd9ae7e0403e8b55d59bfc328389a
rashokanand <ashok.anand91@gmail.com>
2026-02-15 20:48:03 +0530

    Create operate holder function

--- javascript.js
+++ javascript.js
@@ -13,3 +13,12 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let num1, num2, operator;
+
+function operate(operator, num1, num2) {
+    if (operator === "+") return add(num1, num2);
+    if (operator === "-") return subtract(num1, num2);
+    if (operator === "*") return multiply(num1, num2);
+    if (operator === "/") return divide(num1, num2);
+}
