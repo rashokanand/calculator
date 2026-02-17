79fef38ff545edc933a331d4b73809abe3f50e7c
rashokanand <ashok.anand91@gmail.com>
2026-02-17 11:16:52 +0530

    Fix incorrect capture of target area for listener
    Used regex initially to filter out target areas and fill the display
    with the text content of the click target. But this incorrectly
    replicates the entire container or number row or display, when clicked,
    and the whole text content is re-entered into the display.
    Now there are separate event listeners for each button.

--- javascript.js
+++ javascript.js
@@ -26,13 +26,21 @@ function operate(operator, num1, num2) {
 //Update number variables and operator
 const container = document.querySelector(".container");
 const display = document.querySelector(".display");
-container.addEventListener("click", e => {
-    if (/[0-9]/g.test(e.target.textContent) && e.target != display) {
-        console.log("num was clicked");
-        display.textContent += e.target.textContent;
-    }
-    if (/[+\-x=÷]/g.test(e.target.textContent) && e.target != display) {
-        display.textContent += e.target.textContent;
-    }
-    if (e.target.textContent === "AC") display.textContent = "";
-})
+const numbers = document.querySelectorAll(".numbers");
+
+numbers.forEach(addEventListener("click", e => {    
+    display.textContent += (e.target.textContent === "0")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "1")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "2")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "3")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "4")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "5")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "6")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "7")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "8")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "9")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "+")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "-")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "x")? e.target.textContent:"";
+    display.textContent += (e.target.textContent === "÷")? e.target.textContent:"";
+}))
