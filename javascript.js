a8af1997ab1bb8749955ff01bbb798f597b3b431
rashokanand <ashok.anand91@gmail.com>
2026-02-18 11:25:19 +0530

    Allow minus before numbers

--- javascript.js
+++ javascript.js
@@ -29,40 +29,11 @@ const display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".numbers");
 
 container.addEventListener("click", e => {
-    //Display numbers and operators when clicked    
-    display.textContent += (e.target.textContent === "0")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "1")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "2")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "3")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "4")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "5")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "6")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "7")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "8")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "9")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "+")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "-")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "x")? e.target.textContent:"";
-    display.textContent += (e.target.textContent === "÷")? e.target.textContent:"";
-
-    //Clear display
-    (e.target.textContent === "AC")? display.textContent = "":0;
     
-    
-    if (e.target.textContent === "=") {
-
-        //Capture numbers and operator into variables
-        let expr = display.textContent;
-        let arr = expr.split(/[+x÷\-]/,2); //split expression on operator
-        [num1, num2] = [+arr[0],+arr[1] ]; //Convert to integers
-
-        operator = expr.at(expr.search(/[+x÷\-]/));
-        //Handle divide and multiply operators
-        operator = (operator === 'x')? "*":
-        (operator === "÷")? "/": operator;
-
-        //Operate
-        let ans = operate(operator,num1, num2);
-        display.textContent = ans;
+    //Allow entering '-' when display is empty
+    if (display.textContent === '' && e.target.textContent === '-') {
+        display.textContent = '-';
     }
+    
+
 })
