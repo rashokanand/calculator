b1b299341d42168d11be6a35d0d2f16375c58366
rashokanand <ashok.anand91@gmail.com>
2026-02-18 15:35:02 +0530

    Start new calc after prev calculation
    When an expression is evaluated and the answer is displayed, re entering
    any digit must clear the screen and it should start afresh. This
    has been implemented in this commit.

--- javascript.js
+++ javascript.js
@@ -30,11 +30,14 @@ const numberBtns = document.querySelectorAll(".numbers");
 const operatorBtns = document.querySelectorAll(".operator");
 const ansBtn = document.querySelector(".ans");
 const clearBtn = document.querySelector(".clear");
-const isAns = false; //flag to clear display for new operation
+let isAns = false; //flag to clear display for new operation
 
 numberBtns.forEach(el => el.addEventListener("click", e => {
     if (!isAns) display.textContent += e.target.textContent;
-    else display.textContent = e.target.textContent;
+    else {
+        display.textContent = e.target.textContent;
+        isAns = false;
+    }
     }
 )
 )
@@ -51,8 +54,12 @@ operatorBtns.forEach(el => el.addEventListener("click", e => {
     if (!operatorString.includes(display.textContent.at(-1)) && display.textContent != "") {
         displayAns(display.textContent);
         display.textContent += e.target.textContent;
+        isAns = false; //Change flag to take digits as input after operator
     } else if (operatorString.includes(display.textContent.at(-1)) && display.textContent != "-")
-        display.textContent = display.textContent.slice(0,-1) + e.target.textContent;
+        {
+            display.textContent = display.textContent.slice(0,-1) + e.target.textContent;
+            isAns = false;
+        }
 }
 ))
 
@@ -82,4 +89,5 @@ function displayAns(expr) {
     else {
     display.textContent = operate(operator, num1, num2).toFixed(4); // round result
     }
+    isAns = true;
 }
